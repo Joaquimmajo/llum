76652947b678ce7720072affbd25c0b3344ea1d5
Joaquimmajo <82875861+Joaquimmajo@users.noreply.github.com>
2021-04-20 12:18:03 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,32 @@
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    `)
+let llum = 38
+led.setBrightness(llum)
+serial.setBaudRate(BaudRate.BaudRate115200)
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.A)) {
+        if (llum < 255) {
+            llum += 1
+            led.setBrightness(llum)
+            basic.pause(100)
+            serial.writeLine("" + (llum))
+        } else {
+            soundExpression.giggle.play()
+        }
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        if (llum > 0) {
+            llum += -1
+            led.setBrightness(llum)
+            basic.pause(100)
+            serial.writeLine("" + (llum))
+        } else {
+            soundExpression.giggle.play()
+        }
+    }
 })
